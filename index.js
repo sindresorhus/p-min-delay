471c492464bea18cb7b13e24b3d62f2b403fb8e5
fisker Cheung <lionkay@gmail.com>
2021-08-19 21:30:42 +0800

    Simplify (#21)

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 import delay from 'yoctodelay';
 
 export default async function pMinDelay(promise, minimumDelay, {delayRejection = true} = {}) {
-	let promiseError;
+	await Promise[delayRejection ? 'allSettled' : 'all']([
+		promise,
+		delay(minimumDelay)
+	]);
 
-	if (delayRejection) {
-		// TODO: Use try/catch here.
-		// eslint-disable-next-line promise/prefer-await-to-then
-		promise = promise.catch(error => {
-			promiseError = error;
-		});
-	}
-
-	const [value] = await Promise.all([promise, delay(minimumDelay)]);
-
-	if (promiseError) {
-		throw promiseError;
-	}
-
-	return value;
+	return promise;
 }
