91a943ffd7a9d17a0d24b5109b63fa4dab039d77
fisker Cheung <lionkay@gmail.com>
2021-11-26 09:55:37 +0800

    Remove unnecessary `Promise.allSettled`

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import delay from 'yoctodelay';
 
 export default async function pMinDelay(promise, minimumDelay, {delayRejection = true} = {}) {
-	await Promise[delayRejection ? 'allSettled' : 'all']([
-		promise,
-		delay(minimumDelay)
-	]);
-
+	const delayPromise = delay(minimumDelay);
+	await (delayRejection ? delayPromise : Promise.all([promise, delayPromise]));
 	return promise;
 }
